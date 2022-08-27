e718de549339775d13a368d8ac9c87bebaab4171
Reaper <ahoy@barelyhuman.dev>
2022-08-28 00:44:26 +0530

    add each character based tokenization
    also adds the supported syntax as example

--- src/mole.nim
+++ src/mole.nim
@@ -1,77 +1,188 @@
-import std/[strutils]
+import std/[strutils, re]
+
+var variableCharacters = re"[a-zA-Z]"
+
+var keywords = [
+    "def",
+    "loop",
+    "print"
+]
 
 type
     TokenType = enum
         leftBracket,
         rightBracket,
-        varDefinition,
-        comma,
         identifier,
         literal,
         stringLiteral,
         funcDef,
+        varDef,
         loopDef,
-        eof,
-        newLine,
+        printDef,
     Token = object
         value: string
         tokenType: TokenType
 
 var tokens: seq[Token]
+var
+    id = ""
+    strLiteral = ""
+    stringStack: seq[string]
+    bracesStack: seq[string]
+    flowerStack: seq[string]
 
-proc lexer(line: string) =
-    var spaceParts = line.split()
-    for part in spaceParts:
-        if part == "(" or part == "{":
+proc debug(msg: string) =
+    # echo "[debug]"&msg
+    return
+
+proc isKeyword(identifier: string): bool =
+    for keywd in keywords:
+        if identifier == keywd:
+            return true
+    return false
+
+proc handleKeywordIdentifiers(identifier: string) =
+    case identifier:
+        of "loop":
             tokens.add(
                 Token(
-                    value: part,
-                    tokenType: leftBracket
-                )
-            )
-        elif part == ")" or part == "}":
-            tokens.add(
-                Token(
-                    value: part,
-                    tokenType: rightBracket
-                )
-            )
-        elif part == ":=":
-            tokens.add(
-                Token(
-                    value: part,
-                    tokenType: varDefinition
-                )
-            )
-        elif part == "def":
-            tokens.add(
-                Token(
-                    value: part,
-                    tokenType: funcDef
-                )
-            )
-        elif part == "loop":
-            tokens.add(
-                Token(
-                    value: part,
+                    value: id,
                     tokenType: loopDef
                 )
             )
-        elif part.startsWith("\"") and part.endsWith("\""):
+        of "print":
             tokens.add(
                 Token(
-                    value: part.replace("\"",""),
-                    tokenType: stringLiteral
+                    value: id,
+                    tokenType: printDef
+                )
+            )
+        of "def":
+            tokens.add(
+                Token(
+                    value: id,
+                    tokenType: funcDef
                 )
             )
         else:
-            tokens.add(
-                Token(
-                    value: part,
-                    tokenType: identifier
+            return
+
+proc characterAnalyse(line: string) =
+    if len(line) == 0:
+        return
+
+    for i, chr in line:
+        debug $chr
+        case chr:
+            of '"':
+                debug "found string declaration"
+                if stringStack.len > 0:
+                    discard stringStack.pop()
+                    tokens.add(
+                        Token(
+                            value: strLiteral,
+                            tokenType: stringLiteral
+                        )
+                    )
+                    strLiteral = ""
+                else:
+                    stringStack.add($chr)
+            of '(':
+                debug "adding to braces"
+                bracesStack.add($chr)
+                tokens.add(
+                    Token(
+                        value: $chr,
+                        tokenType: leftBracket
+                    )
                 )
-            )
-    return
+
+            of '{':
+                debug "adding flower brace"
+                flowerStack.add($chr)
+                debug $flowerStack.len
+                tokens.add(
+                    Token(
+                        value: $chr,
+                        tokenType: leftBracket
+                    )
+                )
+
+            of ')':
+                debug "found ending brace,popping"
+                if bracesStack.len == 0:
+                    quit "error at" & line
+                discard bracesStack.pop()
+                tokens.add(
+                    Token(
+                        value: $chr,
+                        tokenType: rightBracket
+                    )
+                )
+
+            of '}':
+                debug "found ending flower brace,popping"
+                debug $flowerStack
+                if flowerStack.len == 0:
+                    quit "error at" & line
+                discard flowerStack.pop()
+                tokens.add(
+                    Token(
+                        value: $chr,
+                        tokenType: rightBracket
+                    )
+                )
+            of ':':
+                if i+1 <= line.len-1 and line[i+1] == '=':
+                    tokens.add(
+                        Token(
+                            value: ":=",
+                            tokenType: varDef
+                        )
+                    )
+            else:
+
+                # possibly inside a string, let it take up
+                # everything till the end of string
+                if stringStack.len > 0:
+                    strLiteral = strLiteral & $chr
+                    debug "strLiteral:" & strLiteral
+
+                # if not then check if it's a variable that's
+                # being used, mark as an identifier
+                elif match($chr, variableCharacters):
+                    id = id & chr
+                    if i+1 > line.len-1:
+                        if isKeyword(id):
+                            handleKeywordIdentifiers(id)
+                            id = ""
+                        else:
+                            tokens.add(
+                                Token(
+                                    value: id,
+                                    tokenType: identifier
+                                )
+                            )
+                            id = ""
+                    elif not match($line[i+1], variableCharacters):
+                        if isKeyword(id):
+                            handleKeywordIdentifiers(id)
+                            id = ""
+                        else:
+                            tokens.add(
+                                Token(
+                                    value: id,
+                                    tokenType: identifier
+                                )
+                            )
+                            id = ""
+
+
+
+
+
+
+
 
 
 proc main() =
@@ -86,8 +197,9 @@ proc main() =
             # ignore comment parsing for now
             continue
         else:
-            lexer(line)
+            characterAnalyse(line)
 
-    echo tokens
+    for tok in tokens:
+        echo tok
 
 main()
